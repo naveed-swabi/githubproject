ea80d20f9ce6c8120c0dc42907a71b46ba89b079
naveed-swabi <khanswabi194@gmail.com>
2026-06-29 17:58:58 +0500

    add java class

--- WebApplication1/subject.cs
+++ WebApplication1/subject.cs
@@ -4,5 +4,6 @@
     {
         Console.writeline("class ist will be c#");
         console.writeline("2nd class will be fundamental of computer");
+        console.writeline("3rd class will be java");
     }
 }
