b11e61a00e2dd952f2f7650553eec5f541ecb8ee
naveed-swabi <khanswabi194@gmail.com>
2026-06-30 17:05:17 +0500

    adding discreath math subject.cs
    adding discreath math subject.cs

--- WebApplication1/subject.cs
+++ WebApplication1/subject.cs
@@ -5,5 +5,6 @@
         Console.writeline("class ist will be c#");
         console.writeline("2nd class will be fundamental of computer");
         console.writeline("3rd class will be java");
+        console.writeline("4th class will be discreath math)
     }
 }
